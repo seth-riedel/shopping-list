a4cce8ade1e949699616922d58a807b2a5bb54e9
Seth Riedel <sethcriedel@gmail.com>
2021-08-28 15:28:28 -0500

    Added material UI and basic layout

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,24 @@
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import ShoppingList from './components/ShoppingList';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">
+          SHOPPING LIST
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Container maxWidth="lg">
+      <ShoppingList />
+    </Container>
+  </>
+);
 
 export default App;
---
+++ ui/src/components/ShoppingList.js
@@ -0,0 +1,9 @@
+import Box from '@material-ui/core/Box';
+
+export const ShoppingList = () => (
+  <Box my={ 4 }>
+    Body here
+  </Box>
+);
+
+export default ShoppingList;
